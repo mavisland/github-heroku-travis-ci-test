ad834637e5d11827e926600d19352fe9d3cbbb95
Tanju Yıldız <ben@tanjuyildiz.com>
2020-03-14 17:05:24 +0300

    Test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
-  res.json({ name: "Tanju", surname: "Yıldız", age: 29, location: "Konya, Turkey" });
+  res.json({
+    name: "Tanju",
+    surname: "Yıldız",
+    age: 29,
+    location: "Konya, Turkey",
+    job: "Frontend Developer"
+  });
 });
 
 module.exports = router;
